0c911606b263720b250768f83a1fa82348757312
louis baptiste <louisvis@ymail.com>
2019-03-29 03:45:58 +0000

    Automatically backed up by Learn

--- db/migrate/20190326051834_create_users.rb
+++ db/migrate/20190326051834_create_users.rb
@@ -1,8 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
+
   def change
     create_table :users do |column|
       column.string :name
       column.string :ingredients
     end
   end
+
+  
 end

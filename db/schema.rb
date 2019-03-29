d56ee52c02640bb9ae52e3a3f01d8b0738db4560
louis baptiste <louisvis@ymail.com>
2019-03-29 03:55:40 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,8 @@
 ActiveRecord::Schema.define(version: 20190326051834) do
 
   create_table "users", force: :cascade do |t|
-    t.string  "username"
-    t.string  "password_digest"
-    t.integer "balance"
+    t.string "name"
+    t.string "ingredients"
   end
 
 end
